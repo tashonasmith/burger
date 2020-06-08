86a9176b5d1449b0edc114a2feea51805e1fb210
Tashona Smith <tashona.smith@gmail.com>
2020-06-07 22:53:24 -0500

    Add JAWSDB_URL connection

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,9 @@
 var mysql = require("mysql");
 
+
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -7,6 +11,7 @@ var connection = mysql.createConnection({
   password: "password",
   database: "burgers_db"
 });
+};
 
 connection.connect(function(err) {
   if (err) {
