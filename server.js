acb7a7fc290560bbbba2a7b4bea2fbcbb44fda00
Tashona Smith <tashona.smith@gmail.com>
2020-06-07 22:32:00 -0500

    Add css

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-app.use(express.static("views"));
+app.use(express.static(__dirname + "/public/"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
